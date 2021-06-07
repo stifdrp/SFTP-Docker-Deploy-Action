0079fe04c2ed1cad09f3dde621cec8efdaa88eb8
Lucas Ferreira <lbrandof@gmail.com>
2021-06-07 12:08:58 -0300

    Modificando as permissões do diretório no script entrypoint.sh e inserindo usuario sysusername no arquivo action.yml

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,13 +14,17 @@ chmod 600 $TEMP_SSH_PRIVATE_KEY_FILE
 
 echo 'ssh start'
 
-ssh -o StrictHostKeyChecking=no -p $3 -i $TEMP_SSH_PRIVATE_KEY_FILE $1@$2 mkdir -p $6
+ssh -o StrictHostKeyChecking=no -p $3 -i $TEMP_SSH_PRIVATE_KEY_FILE $1@$2 sudo mkdir -p $6
+
+ssh -o StrictHostKeyChecking=no -p $3 -i $TEMP_SSH_PRIVATE_KEY_FILE $1@$2 sudo chown gitactions $6
 
 echo 'sftp start'
 # create a temporary file containing sftp commands
 printf "%s" "put -r $5 $6" >$TEMP_SFTP_FILE
 #-o StrictHostKeyChecking=no avoid Host key verification failed.
-sftp -b $TEMP_SFTP_FILE -P $3 $7 -o StrictHostKeyChecking=no -i $TEMP_SSH_PRIVATE_KEY_FILE $1@$2
+sftp -b $TEMP_SFTP_FILE -P $3 $8 -o StrictHostKeyChecking=no -i $TEMP_SSH_PRIVATE_KEY_FILE $1@$2
+
+ssh -o StrictHostKeyChecking=no -p $3 -i $TEMP_SSH_PRIVATE_KEY_FILE $1@$2 sudo chown www-data:$7 $6
 
 echo 'deploy success'
 exit 0
